233a77aea1ea41c990c5a299b99c35417a6a68fe
jamangstangs <jamangsong@gmail.com>
2022-05-28 15:19:09 +0900

    - 생성자 주입

---
+++ Spring Core/PracticeCode1/src/main/java/com/example/demo/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.springframework.context.annotation.ComponentScan.*;
+
+@Configuration
+@ComponentScan
+public class AutoAppConfig {
+}
