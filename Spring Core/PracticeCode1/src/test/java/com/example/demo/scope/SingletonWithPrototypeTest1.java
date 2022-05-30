887c5d351438e06a60c684956e449fac6180b3c5
jamangstangs <jamangsong@gmail.com>
2022-05-30 15:14:38 +0900

    - JSR Provider 도입

--- Spring Core/PracticeCode1/src/test/java/com/example/demo/scope/SingletonWithPrototypeTest1.java
+++ Spring Core/PracticeCode1/src/test/java/com/example/demo/scope/SingletonWithPrototypeTest1.java
@@ -3,12 +3,14 @@ package com.example.demo.scope;
 import ch.qos.logback.core.net.server.Client;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,18 +28,18 @@ public class SingletonWithPrototypeTest1 {
         int count2 = clientBean2.logic();
         assertThat(count2).isEqualTo(2);
     }
-
     static class ClientBean {
         @Autowired
-        private ApplicationContext ac;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
     }
+
 
     static class PrototypeBean {
         private int count = 0;
