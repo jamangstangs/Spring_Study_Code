e93166b32e88ed5649fd7a0c067690f2db6e6b36
jamangstangs <jamangsong@gmail.com>
2022-07-04 20:34:11 +0900

    helloServlet 등록 - Request 확인 - Response 객체 세팅하기

--- Spring_MVC/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Spring_MVC/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,12 +2,14 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	// 해당 패키지와, 하위 패키지에 있는 Servlet을 자동으로 등록해준다.
 @SpringBootApplication
 public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
+
---
+++ Spring_MVC/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Request에서 Parameter 받기
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        // Response 객체 세팅하기
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write("hello " + username);
+    }
+
+}
