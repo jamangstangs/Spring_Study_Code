a6a5d43e4403e8184567dcc64b7f29a5166f0c2e
jamangstangs <jamangsong@gmail.com>
2022-08-08 22:38:14 +0900

    JDBC 예제 구현 완료

--- Spring_DB/jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
+++ Spring_DB/jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -1,4 +1,18 @@
 package hello.jdbc.connection;
 
-public class DBConnectionUtilTest {
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        Assertions.assertThat(connection).isNotNull();
+    }
+
 }
