3ba105bca0f939989e3bffa38cd154be7d9d526c
jamangstangs <jamangsong@gmail.com>
2022-08-24 13:45:52 +0900

    memberService Test code 작성 계좌이체 검증

---
+++ Spring_DB/jdbc/src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,57 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+* 기본 동작, 트랜잭션이 없어서 문제 발생
+* */
+class MemberServiceV1Test {
+
+    public static final String Member_A = "memberA";
+    public static final String Member_B = "memberB";
+    public static final String Member_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(Member_A, 10000);
+        Member memberB = new Member(Member_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        // 이 상황에서는 A는 2000원이 깎이고 B는 2000원이 들어와야한다.
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+
+
+    }
+}
