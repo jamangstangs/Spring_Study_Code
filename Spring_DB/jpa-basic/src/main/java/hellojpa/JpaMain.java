b7fcf09ddc6782ce1dccf50226dbcb7e488163ee
jamangstangs <jamangsong@gmail.com>
2022-09-07 13:17:26 +0900

    Project init : Spring boot & JPA 프로젝트 시작

---
+++ Spring_DB/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setName("name");
+            member.setId(100L);
+
+            System.out.println("-----이전-----");
+            em.persist(member);
+            System.out.println("-----이후-----");
+
+            Member findMember = em.find(Member.class, 100L);
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
