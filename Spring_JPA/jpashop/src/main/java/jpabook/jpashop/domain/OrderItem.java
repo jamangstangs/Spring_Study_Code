9860beae5c3a8c95fc90285f9b802e215e8ce2b1
jamangstangs <jamangsong@gmail.com>
2022-09-15 15:46:14 +0900

    feat: 주문, 주문 상품 엔티티를 개발한다. 여기서 비즈니스 로직과 생성 메서드를 엔티티 레벨에서 구현하였다.

--- Spring_JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Spring_JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -30,4 +30,12 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    // 비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 }
