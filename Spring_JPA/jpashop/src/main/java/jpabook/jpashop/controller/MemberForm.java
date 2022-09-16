b38d16084a6e497b16e5a056b9f0a7393b32a123
jamangstangs <jamangsong@gmail.com>
2022-09-16 23:35:47 +0900

    feat: Member 가입 POST 구현, 여기서 MemberForm에서 NotEmpty 제약을 걸어둔 상태에서 이름을 작성하지 않으면 예외를 자바단에서 던지게 된다. 이때 던지는 에러리르 BindingResult에 담아서 Spring에서 처리할 수 있다. 처리한 에러는 다시 members/createMemberForm 뷰로 가게 되고, 여기서 BindingResult에 담긴 에러 정보를 통해 Thymeleaf에서 에러에 관한 렌더링을 진행한다.

--- Spring_JPA/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ Spring_JPA/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -3,11 +3,13 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class MemberForm {
 
-    @NotNull
+    @NotNull(message = "회원 이름은 필수 입니다")
     private String name;
 
     private String city;
