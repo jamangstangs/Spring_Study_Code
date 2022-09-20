c2112f376f26e9628527d838efd8e07865b787fb
jamangstangs <jamangsong@gmail.com>
2022-09-20 20:04:50 +0900

    feat: List를 반환하면, 확장이 어렵다. 따라서 DTO를 만들어서 DTO에서 아래 두가지를 만족하였다. - 반환 타입을 List -> Object로 변환하고 데이터를 DTO 안에 필드로 저장함. - 필요한 필드만 노출이 가능하다. 엔티티 레벨에서 제한을 거는 것이 아니라.

--- Spring_JPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ Spring_JPA/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +20,25 @@ public class MemberApiController {
         return memberService.findMembers();
     }
 
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> members = memberService.findMembers();
+
+        List<MemberDto> collect = members.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -73,4 +91,11 @@ public class MemberApiController {
 
         }
     }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
 }
