ed6a9c8c76fc63362990fefd3499aad538dabfcc
jamangstangs <jamangsong@gmail.com>
2022-09-23 15:41:08 +0900

    feat: OrderQueryRepository -> 화면이나 API에 의존관계를 떼어내기 위함. -> Entity가 아닌 특정 화면에 대한 쿼리를 내보내기 위해 생성 - Query : 루트1번, 컬렉션 N번 실행 - To One 관계들을 먼저 조회, ToMany는 별도로 처리 - 결과적으로는 n+1 문제이다. : findOrderQueryDtos - findOrders 1번, result.반복하면서 쿼리 2번 나감.

--- Spring_JPA/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ Spring_JPA/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -30,7 +33,7 @@ public class OrderApiController {
         for (Order order : all) {
             order.getMember().getName();
             order.getDelivery().getAddress();
-            List<OrderItem> orderItems = order.getOrderItems();  
+            List<OrderItem> orderItems = order.getOrderItems();
             orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
@@ -66,7 +69,10 @@ public class OrderApiController {
         return result;
     }
 
-
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
 
     @Data
     static class OrderDto {
