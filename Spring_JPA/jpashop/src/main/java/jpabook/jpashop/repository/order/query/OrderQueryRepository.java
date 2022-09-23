dfea29e76dbf505b516aa99d7b554ebd65b30856
jamangstangs <jamangsong@gmail.com>
2022-09-23 16:42:05 +0900

    feat: V5Orders -> 쿼리 한 번 날리고, 가져온 값을 메모리에서 map에 집어넣고 또 한번 쿼리를 날려서 가져온다. - ToOne 관계들을 먼저 조인하고, 얻은 식별자 orderId로 ToMany 관계인 OrderItem을 한꺼번에 조회한다. - Map을 사용해서 매칭성능 향상

--- Spring_JPA/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ Spring_JPA/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -47,4 +49,37 @@ public class OrderQueryRepository {
         ).getResultList();
     }
 
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        // 주문 번호가 뽑혀 잇을 것이다.
+        List<Long> orderIds = toOrderIds(result);
+        // 한 방에 가져올 것이다.
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(orderIds);
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery("select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count" +
+                        ")" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+        // 한방에 쿼리해온 것이다.  한번 최적화 해주자.
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        return orderItemMap;
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(o -> o.getOrderId()).collect(Collectors.toList());
+    }
 }
